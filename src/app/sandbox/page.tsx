29753bd41936983d1b8263ae874da49b5193505f
Jakub Fronczyk <jakubfronczyk@gmail.com>
2025-03-23 12:28:46 +0100

    refacotr: add delete data for now

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { mockFiles, mockFolders } from "~/lib/mock-data";
 import { db } from "~/server/db";
 import { files, folders } from "~/server/db/schema";
@@ -15,19 +16,26 @@ export default function SandboxPage() {
             mockFolders.map((folder, index) => ({
               id: index + 1,
               name: folder.name,
-              parent: index !== 0 ? index : null,
+              parent: index !== 0 ? 1 : null,
             })),
           );
 
           await db.insert(files).values(
-            mockFiles.map((files, index) => ({
+            mockFiles.map((file, index) => ({
               id: index + 1,
-              name: files.name,
+              name: file.name,
               size: 50000,
-              url: files.url,
+              url: file.url,
               parent: (index % 3) + 1,
             })),
           );
+          // await db.delete(files).where(eq(files.id, files.id)); // This deletes all files
+
+          // // Deleting all folders
+          // await db.delete(folders).where(eq(folders.id, folders.id));
+
+          // console.log(folderInsert);
+          // console.log(fileInsert);
         }}
       >
         <button type="submit">Seed</button>
