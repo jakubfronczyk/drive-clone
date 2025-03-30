ad6a8544b0e97057111d7a1cb2d25be4209458d7
Jakub Fronczyk <jakubfronczyk@gmail.com>
2025-03-30 20:02:51 +0200

    fix: add correct schema

--- src/server/db/schema/file.ts
+++ src/server/db/schema/file.ts
@@ -1,14 +1,16 @@
 import { index, integer, varchar } from "drizzle-orm/pg-core";
 import { createTable } from "../schema";
 
-export const folder_table = createTable(
-  "folders_table",
+export const files_table = createTable(
+  "files_table",
   {
     id: integer("id").notNull().primaryKey(),
     name: varchar("name", { length: 255 }).notNull(),
-    parent: integer("parent"),
+    url: varchar("url", { length: 255 }).notNull(),
+    parent: integer("parent").notNull(),
+    size: integer("size").notNull(),
   },
-  (folder) => ({
-    parentIdx: index("folders_parent_idx").on(folder.parent),
+  (file) => ({
+    parentIdx: index("files_parent_idx").on(file.parent),
   }),
 );
